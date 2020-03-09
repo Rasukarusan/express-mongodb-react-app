1996789a3b117490ca6f4a14b43624b7e1e78d38
Tanaka Naoto <gta_youtube@yahoo.co.jp>
2020-03-09 21:56:21 +0900

    Move controller's function to service layer.
    refs #master

--- api/src/controllers/orderController.ts
+++ api/src/controllers/orderController.ts
@@ -1,56 +1,54 @@
 import { Request, Response } from "express";
-import mongoose = require('mongoose');
-import Order from '../models/orders';
+import OrderService from '../services/orderService';
+const service = new OrderService();
 
-exports.index = function(req: Request, res: Response) {
-  Order.find(function(err, orders) {
-    if (err) return console.log(err);
-    orders.map(order => {
-      console.log(order.name);
-      console.log(order.age);
-    })
-  });
-  return res.send('this is controller!!!');
-}
-
-exports.show = function(req: Request, res: Response) {
-  console.log(req.params.id);
-  let id = req.params.id;
-  Order.findById(id, function(err, order) {
-    console.log(order);
-  })
-  return res.send('Parameter: ' + req.params.id);
-}
-
-exports.store = function(req: Request, res: Response) {
-  if (!req.files) {
-    return;
+exports.index = async function(req: Request, res: Response) {
+  try {
+    let orders = await service.getAll();
+    return res.send(orders);
+  } catch (e) {
+    return res.status(400).json({ status: 400, message: e.message });
   }
-  // Read uploaded files
+}
+
+exports.show = async function(req: Request, res: Response) {
+  let id = req.params.id;
+  try {
+    let order = await service.getById(id);
+    return res.send(order);
+  } catch (e) {
+    return res.status(400).json({ status: 400, message: e.message });
+  }
+}
+
+exports.store = async function(req: Request, res: Response) {
+  if (!req.files) {
+    return res.status(204).json({msg: 'Not found files. Please upload files. '})
+  }
   let files = req.files as Express.Multer.File[];
-  files.map((file: Express.Multer.File) => {
-    console.log(file);
-  });
-  // Save to DB
-  Order.insertMany([
-    { name: 'hoge', age: 11 },
-    { name: 'fooo', age: 12 },
-  ], (err) => {
-    if (err) return res.send(err);
-  });
-  return res.status(200).json({msg: 'Upload OK!'})
+  try {
+    let order = await service.save(files);
+    return res.status(200).json({msg: 'Upload OK!', record: order})
+  } catch (e) {
+    return res.status(400).json({ status: 400, message: e.message });
+  }
 }
 
-exports.update = function(req: Request, res: Response) {
-  Order.updateOne({name: 'hoge'}, {name: 'hogehoge'}, (err, row) => {
-    if (err) return res.send(err);
-    return res.send(row);
-  });
+exports.update = async function(req: Request, res: Response) {
+  try {
+    let orders = await service.update();
+    return res.send(orders);
+  } catch (e) {
+    return res.status(400).json({ status: 400, message: e.message });
+  }
 }
 
-exports.destroy = function(req: Request, res: Response) {
-  Order.deleteOne({name: 'tanak', age: 999}, (err) => {
-    if (err) return res.send(err);
-  })
-  return res.send('Parameter of Delete: ' + req.params.id);
+exports.destroy = async function(req: Request, res: Response) {
+  try {
+    let where = { name: 'hoge', age: 11 };
+    let orders = await service.delete(where);
+    return res.send(orders);
+  } catch (e) {
+    return res.status(400).json({ status: 400, message: e.message });
+  }
 }
---
+++ api/src/services/orderService.ts
@@ -0,0 +1,50 @@
+import Order from '../models/orders';
+
+export default class OrderService {
+
+  public async getAll() {
+    try {
+      return Order.find().exec();
+    }catch(err) {
+      throw err;
+    }
+  }
+
+  public async getById(id: string) {
+    try {
+      return Order.findById(id).exec();
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  public async save(files: Express.Multer.File[]) {
+    files.map((file: Express.Multer.File) => {
+      console.log(file);
+    });
+    try {
+      return Order.insertMany([
+        { name: 'hoge', age: 11 },
+        { name: 'fooo', age: 12 },
+      ]);
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  public async update() {
+    try {
+      return Order.updateMany({ name: 'hoge' }, { name: 'hogehoge' });
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  public async delete(where: { [key: string] : any }) {
+    try {
+      return Order.deleteMany(where);
+    } catch (e) {
+      throw e;
+    }
+  }
+}
