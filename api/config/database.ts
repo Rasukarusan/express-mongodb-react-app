16b79a697274fc0b85bc0475b73e140e4ba109e4
Tanaka Naoto <gta_youtube@yahoo.co.jp>
2020-03-11 00:05:14 +0900

    Fix database connect.
    refs #master

--- api/config/database.ts
+++ api/config/database.ts
@@ -1,11 +1,16 @@
 import mongoose = require('mongoose');
-mongoose.connect('mongodb://mongodb:27017/test', {useNewUrlParser: true});
-mongoose.connection.on('error', error => {
+
+const options = {
+  useNewUrlParser: true, 
+};
+mongoose.connect('mongodb://mongodb:27017/test', options);
+
+const db = mongoose.connection;
+db.on('error', error => {
   console.log('DB接続エラー: ', error);
 });
-mongoose.connection.once('open', () => {
+db.once('open', () => {
   console.log('DB接続OK!!');
 })
 
-const db = mongoose.connection;
 export default db;
