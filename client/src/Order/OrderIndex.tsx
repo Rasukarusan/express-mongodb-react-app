4510538813daa3a675b9d3ab39e1bc1601e1b651
Tanaka Naoto <gta_youtube@yahoo.co.jp>
2020-03-15 22:00:05 +0900

    Add OrderList View. - Show API response on table - Filter table function
    refs #master

--- client/src/Order/OrderIndex.tsx
+++ client/src/Order/OrderIndex.tsx
@@ -1,38 +1,72 @@
 import React, { useState, useEffect }  from 'react';
-import styled, { css } from 'styled-components'
+import { Pane, Heading, Table } from 'evergreen-ui';
+import axios from 'axios';
+import { filter } from 'fuzzaldrin-plus'
 
-const Container = styled.div`
-  color: black;
-  background: #ffffff;
-  width: 100vw;
-  height:100vh;
-`;
+interface IOrder {
+  _id: string,
+  name: string,
+  age: number
+}
 
 function List() {
-  const [apiResponse, setApiResponse] = useState<String>('');
+  const [apiResponse, setApiResponse] = useState<IOrder[]>([]);
+  const [searchQuery, setSearchQuery] = useState<string>('');
 
-  const callAPI = () => {
-    fetch('http://localhost:9000/orders')
-      .then(res => res.text())
-      .then(res => setApiResponse(res))
+  const getOrders = () => {
+    axios.get<IOrder[]>('http://localhost:9000/orders')
+      .then(res => {
+        setApiResponse(res.data);
+      })
       .catch(err => err);
   }
 
   useEffect(() => {
-    callAPI();
+    getOrders();
   }, []);
 
+  const renderRow = (order: IOrder) => {
+      return (
+        <Table.Row key={order._id}>
+          <Table.TextCell>{ order.name }</Table.TextCell>
+          <Table.TextCell isNumber>{ order.age }</Table.TextCell>
+        </Table.Row>
+      );
+  }
+
+  const filterTable = (orders) : IOrder[] =>  {
+    const query = searchQuery.trim();
+    if (query.length === 0) return orders;
+    return orders.filter(order => {
+      const result = filter([order.name], query);
+      return result.length === 1;
+    });
+  }
+
   return(
-    <div> {apiResponse} </div>
+    <div>
+      <Table>
+        <Table.Head>
+          <Table.SearchHeaderCell
+            onChange={ value => setSearchQuery(value) }
+            value={ searchQuery }
+          />
+          <Table.TextHeaderCell>age</Table.TextHeaderCell>
+        </Table.Head>
+        <Table.VirtualBody height={320}>
+          { filterTable(apiResponse).map((order) => renderRow(order)) }
+        </Table.VirtualBody>
+      </Table>
+    </div>
   );
 }
 
 function OrderIndex() {
   return (
-    <Container>
-      <h1>Hello Order Index!!</h1>
+    <Pane>
+      <Heading is="h1" size={900}>Hello Order Index!!</Heading>
       <List />
-    </Container>
+    </Pane>
   );
 }
 
