42063c88f7dda1e057f9e02926dd10befeeee3b6
Tanaka Naoto <gta_youtube@yahoo.co.jp>
2020-03-19 01:25:54 +0900

    Update sidebar layout
    refs #master

--- client/src/SideBar.tsx
+++ client/src/SideBar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components'
+import { Avatar } from 'evergreen-ui';
 
 const Nav = styled.div`
   height: 100%;
@@ -17,7 +18,6 @@ const Nav = styled.div`
 const NavItem = styled(Link)`
   padding: 6px 8px 6px 16px;
   text-decoration: none;
-  font-size: 25px;
   color: #818181;
   display: block;
   &:hover { color: #f1f1f1; }
@@ -26,9 +26,14 @@ const NavItem = styled(Link)`
 function SideBar() {
   return (
     <Nav>
+      <NavItem to="/">
+        <Avatar name="N E" isSolid color="green" size={32} />
+      </NavItem>
       <NavItem to="/">Home</NavItem>
-      <NavItem to="/CsvUpload">CsvUpload</NavItem>
-      <NavItem to="/OrderIndex">OrderList</NavItem>
+      <hr />
+      <NavItem to="/CsvUpload">受注一括登録</NavItem>
+      <hr />
+      <NavItem to="/OrderIndex">受注伝票</NavItem>
     </Nav>
   );
 }
